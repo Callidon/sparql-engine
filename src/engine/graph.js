39c6e9ca0342e37ea7c4ba78a26ba48d10793407
Thomas Minier <tminier01@gmail.com>
2018-08-24 15:30:46 +0200

    Graoh now only requires a find method to provides a default evalBGP impl

--- src/engine/graph.js
+++ src/engine/graph.js
@@ -24,6 +24,8 @@ SOFTWARE.
 
 'use strict'
 
+const { single } = require('asynciterator')
+const TripleOperator = require('../operators/triple-operator.js')
 /**
  * An abstract RDF Graph, accessed through a RDF Dataset
  * @abstract
@@ -50,7 +52,7 @@ class Graph {
    * @param  {string}   triple.object - RDF triple's object
    * @return {Promise} A Promise fullfilled when the insertion has been completed
    */
-  insert (triple, done) {
+  insert (triple) {
     throw new Error('A Graph must implements an "insert" method to support SPARQL INSERT queries')
   }
 
@@ -60,10 +62,22 @@ class Graph {
    * @param  {string}   triple.subject - RDF triple's subject
    * @param  {string}   triple.predicate - RDF triple's predicate
    * @param  {string}   triple.object - RDF triple's object
-   * @return {Promise} A Promose fullfilled when the deletion has been completed
+   * @return {Promise} A Promise fullfilled when the deletion has been completed
    */
-  delete (triple, done) {
+  delete (triple) {
     throw new Error('A Graph must implements a "delete" method to support SPARQL DELETE queries')
+  }
+
+  /**
+   * Returns an iterator that finds RDF triples matching a triple pattern in the graph.
+   * @param  {Object}   triple - Triple pattern to find
+   * @param  {string}   triple.subject - Triple pattern's subject
+   * @param  {string}   triple.predicate - Triple pattern's predicate
+   * @param  {string}   triple.object - Triple pattern's object
+   * @return {AsyncIterator} An iterator which finds RDF triples matching a triple pattern
+   */
+  find (triple, options) {
+    throw new Error('A Graph must implements either a "find" or an "evalBGP" method to support SPARQL queries')
   }
 
   /**
@@ -73,7 +87,9 @@ class Graph {
    * @return {AsyncIterator} An iterator which evaluates the Basic Graph pattern on the Graph
    */
   evalBGP (bgp, options) {
-    throw new Error('A Graph must implements an "evalBGP" method to support SPARQL queries')
+    return bgp.reduce((iter, triple) => {
+      return new TripleOperator(iter, triple, this, options)
+    }, single({}))
   }
 }
 
