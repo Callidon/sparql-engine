d6eb4304f95626b00887699c93fb87bc4c8fa317
Thomas Minier <tminier01@gmail.com>
2018-08-30 10:02:34 +0200

    refactor tests folder structure

--- tests/modifiers/select-test.js
+++ tests/modifiers/select-test.js
@@ -1,4 +1,4 @@
-/* file : base-test.js
+/* file : select-test.js
 MIT License
 
 Copyright (c) 2018 Thomas Minier
@@ -27,14 +27,37 @@ SOFTWARE.
 const expect = require('chai').expect
 const { getGraph, TestEngine } = require('../utils.js')
 
-describe('Basic SPARQL queries', () => {
+describe('SELECT SPARQL queries', () => {
   let engine = null
   before(() => {
     const g = getGraph('./tests/data/dblp.nt')
     engine = new TestEngine(g)
   })
 
-  it('should evaluate simple SPARQL queries', done => {
+  it('should evaluate simple SELECT SPARQL queries', done => {
+    const query = `
+    PREFIX dblp-pers: <https://dblp.org/pers/m/>
+    PREFIX dblp-rdf: <https://dblp.uni-trier.de/rdf/schema-2017-04-18#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    SELECT ?name ?article WHERE {
+      ?s rdf:type dblp-rdf:Person .
+      ?s dblp-rdf:primaryFullPersonName ?name .
+      ?s dblp-rdf:authorOf ?article .
+    }`
+    const results = []
+    const iterator = engine.execute(query)
+    iterator.on('error', done)
+    iterator.on('data', b => {
+      expect(b).to.have.keys('?name', '?article')
+      results.push(b)
+    })
+    iterator.on('end', () => {
+      expect(results.length).to.equal(5)
+      done()
+    })
+  })
+
+  it('should evaluate SELECT * SPARQL queries', done => {
     const query = `
     PREFIX dblp-pers: <https://dblp.org/pers/m/>
     PREFIX dblp-rdf: <https://dblp.uni-trier.de/rdf/schema-2017-04-18#>
