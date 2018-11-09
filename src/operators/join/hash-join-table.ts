aa4d5498da4f23ddbfea4c1376a40bce6c1d7197
Thomas Minier <tminier01@gmail.com>
2018-11-09 09:50:38 +0100

    typos

--- src/operators/join/hash-join-table.ts
+++ src/operators/join/hash-join-table.ts
@@ -26,7 +26,7 @@ import { Bindings } from '../../rdf/bindings'
 
 /**
  * A HashJoinTable is used by a Hash-based join to save set of bindings corresponding to a joinKey.
- * All bindings correspoding to the save value of the joinKey are aggregated in a list.
+ * All bindings corresponding to the save value of the joinKey are aggregated in a list.
  */
 export default class HashJoinTable {
   private readonly _content: Map<string, Bindings[]>
@@ -48,7 +48,8 @@ export default class HashJoinTable {
   }
 
   /**
-   * Peform a join between a set of bindings and all set of bindings in the table associated with the key. Returns an empty list if there is no join results.
+   * Perform a join between a set of bindings and all set of bindings in the table associated with the key.
+   * Returns an empty list if there is no join results.
    * @param  key  - Key used to fetch set of set of bindings
    * @param  bindings - Bindings to join with
    * @return Join results, or an empty list if there is none.
